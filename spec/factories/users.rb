dda52aa3d2e4b5b7f0b8da2741ad7390d315ef4e
27kan <79520951+27kan@users.noreply.github.com>
2021-05-04 12:11:11 +0900

    フェイクデータの作成
    ・Faker,Gimeiによる作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    email                 {'test@test.com'}
-    password              {'test1111'}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    nickname              {'test'}
-    last_name             {'てすと'}
-    first_name            {'たろう'}
-    last_reading          {'テスト'}
-    first_reading         {'タロウ'}
-    birthday              {'2000年5月3日'}
+    nickname              {Faker::Name.name}
+    last_name             {Faker::Name.last_name}
+    first_name            {Faker::Name.first_name}
+    last_reading          {Gimei.name.katakana}
+    first_reading         {Gimei.name.katakana}
+    birthday              {Faker::Date.birthday}
   end
 end
