85e13fb73dea5c30fa414310d3f29c64a5ad8468
27kan <79520951+27kan@users.noreply.github.com>
2021-05-04 14:03:27 +0900

    名前をFaker→Gimeiへ
    テスト時不便なのでGimeiへ変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,11 +3,11 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    nickname              {Faker::Name.name}
-    last_name             {Faker::Name.last_name}
-    first_name            {Faker::Name.first_name}
-    last_reading          {Gimei.name.katakana}
-    first_reading         {Gimei.name.katakana}
+    nickname              {Gimei.name}
+    last_name             {Gimei.name.last}
+    first_name            {Gimei.name.first}
+    last_reading          {Gimei.name.katakana.last}
+    first_reading         {Gimei.name.katakana.first}
     birthday              {Faker::Date.birthday}
   end
 end
