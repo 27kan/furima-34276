94093ae333adb4c4411b95188c0b91c508617b79
27kan <79520951+27kan@users.noreply.github.com>
2021-05-06 06:46:45 +0900

    Itemモデルのバリデーション設定
    ・ActiveHashは**_idとして、数値で取り込む
    ・imageも忘れずに

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name, :info, :price
+    validates :category_id,
+              :item_status_id,
+              :shipping_id,
+              :area_id,
+              :schedule_id,
+                numericality:{ other_than: 0 }
+    validates :image
+  end
+
   # ActiveHashのアソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -7,5 +18,6 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :schedule
 
+  has_one_attached :image
   belongs_to :user
 end
