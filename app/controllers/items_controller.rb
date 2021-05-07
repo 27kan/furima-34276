9390bd15e5f8f28d32b23b50fe6eea6f5cf2aad5
27kan <79520951+27kan@users.noreply.github.com>
2021-05-07 18:47:27 +0900

    controllerの実装
    newアクション
    createアクション
    ストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+  def item_params
+    params.require(:item).permit(:image,:name,:info,:category_id,:item_status_id,:shipping_id,:area_id,:schedule_id,:price).merge(user_id: current_user.id)
+  end
+
 end
