589bd00c67d545eab9b4db13ce3afe97d076ce1d
27kan <79520951+27kan@users.noreply.github.com>
2021-05-11 06:40:09 +0900

    JavaScriptコメントの編集

--- app/javascript/price_calc.js
+++ app/javascript/price_calc.js
@@ -3,9 +3,9 @@ function price_calc() {
   const inputPrice = document.getElementById("item-price")
   // 手順2 イベント発火の条件設定
   inputPrice.addEventListener('input',()=>{
-    // 手順3 必要なvalue属性を取得する
+    // 手順3 計算に必要な入力された値（value属性）を取得する
     const inputValue = inputPrice.value;
-    // 手順4 出力先にhtmlを作成してinnerHTMLで返す
+    // 手順4 出力先に計算結果を作成してinnerHTMLで返す
     const addTaxPrice = document.getElementById("add-tax-price")
     const profit = document.getElementById("profit")
     const fee = Math.floor(inputValue * 0.1) // 小数点の整理するメソッド
