21f2f923d15680fea31e0da3c6956f7a3b474c1b
27kan <79520951+27kan@users.noreply.github.com>
2021-05-15 12:41:40 +0900

    correct_userの追加
    URL直接入力で削除ができないようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :set_params, only: [:show, :edit, :update, :destroy]
   
   def index
