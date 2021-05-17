7e067df55734da14620c366812c4cf59aa6a9ffb
27kan <79520951+27kan@users.noreply.github.com>
2021-05-17 13:53:48 +0900

    Formオブジェクトの作成
    ・order_addressモデルの作成
    →アクティブモデルの適用
    →バリデーション設定
    →saveメソッドの定義

---
+++ app/models/order_address.rb
@@ -0,0 +1,20 @@
+class OrderAddress
+    # Formオブジェクトの機能を持たせるため
+    include ActionModel::Model
+    # 保存したいカラム名を属性値として扱えるように
+    # order_idは必要かわからないから必要なら後で付け足す
+    attr_accessor :user_id, :item_id, :postal_code, :area_id, :city, :house_number, :building_name, :phone
+
+    with_options presence: true do
+        validates :user_id, :item_id, :city, :house_number, :phone
+        validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
+        validates :area_id, numericality: {other_than: 0 }
+    end
+    
+    def save
+        order = Order.create(user_id: user_id, item_id: item_id)
+        Address.create(postal_code: postal_code, area_id: area_id, city: city
+                        house_number: house_number, building_name: building_name
+                        phone: phone, order_id: order.id)
+    end
+end
