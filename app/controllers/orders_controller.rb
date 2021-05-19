29d81d2e20023214f27076d398c4a618fe8df033
27kan <79520951+27kan@users.noreply.github.com>
2021-05-19 14:46:08 +0900

    コントローラーの実装
    ・indexアクション
    →インスタンス変数を渡す
    
    ・createアクション
    →保存
    
    ・before_action
    URL直接入力でのアクセス制限
    →authenticate_user!
    →correct_user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,32 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user
+
+  def index # indexアクションでインスタンスを渡す
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :area_id, :city, :house_number, :building_name, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def correct_user # 出品者を自身の商品購入画面へ遷移させない
+    @user = Item.find(params[:item_id])
+    if @user.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
 end
