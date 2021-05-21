46d40d6810f3dc8c3ae5b4147fe85f2c4e2122ed
27kan <79520951+27kan@users.noreply.github.com>
2021-05-21 15:53:20 +0900

    モデル単体テスト実装
    ・FactoryBotの設定
    ・モデル単体テストの実装

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :order_address do
-    
+    postal_code   { Faker::Number.number(digits: 7).to_s.insert(3, "-") }
+    area_id       { Faker::Number.within(range: 1..47 )}
+    city          { Gimei.city.kanji }
+    house_number  { Gimei.town.kanji }
+    building_name { Gimei.town.kanji }
+    phone         { Faker::Number.number(digits: 11)}
   end
 end
