e468a509ca7db0abc39423d77555a28f56398ab7
27kan <79520951+27kan@users.noreply.github.com>
2021-05-26 22:53:48 +0900

    Payjpによる決済処理
    決済処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,6 +11,7 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id])
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -28,5 +29,14 @@ class OrdersController < ApplicationController
     if @user.user_id == current_user.id
       redirect_to root_path
     end
+  end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
