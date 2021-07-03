aaaf0575e0db921e0aeb6fd5a50722197fed1c80
27kan <79520951+27kan@users.noreply.github.com>
2021-07-03 16:08:28 +0900

    aa

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,25 @@
 class LikesController < ApplicationController
+
+  before_action :set_params
+
+  def like
+    like = current_user.likes.new(item_id: @item.id)
+    like.save
+  end
+
+  def unlike
+    like = current_user.likes.find_by(item_id: @item.id)
+    like.destroy
+  end
+
+  private
+  def item_params
+    require.permit
+  end
+
+  def set_params
+    @item = Item.find(params[:item_id])
+    @id_name = "#like-button-#{@item.id}"
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,6 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
   has_many :likes
-  has_many :likes_items, through: :likes, source: :item
-  
+  has_many :like_items, through: :likes, source: :item
+
 end
