34f8ee9beafde15034152a73411bba19068e2f43
27kan <79520951+27kan@users.noreply.github.com>
2021-07-03 15:00:35 +0900

    モデル、コントローラー作成
    アソシエーションを組む

--- app/models/item.rb
+++ app/models/item.rb
@@ -24,4 +24,8 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_one :order
+
+  has_many :likes
+  has_many :liking_users, through: :likes, source: :user
+  
 end
